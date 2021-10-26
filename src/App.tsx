105ee32cb17c6a02d62bab67710b1d9f1f59e3e6
Raimundo Machado <larissainaciom.dacosta@gmail.com>
2021-10-26 18:56:44 -0300

    atualizando blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/estaticos/navbar/NavBar';
 import Home from './paginas/home/Home';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
-    <Home/>
+    <>
+      <NavBar/>
+      <Home/>
+      <Footer/>
+    </>
   );
 }
 
