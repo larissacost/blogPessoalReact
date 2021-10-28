af593fc8a9806cd8143323411d828ddb7063d20b
Raimundo Machado <larissainaciom.dacosta@gmail.com>
2021-10-28 19:32:58 -0300

    atualizando paginas e estilo do blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+
 import './App.css';
+
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import NavBar from './components/estaticos/navbar/NavBar';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar/>
-      <Home/>
+      <Switch>
+        <div style = {{minHeight : '100vh'}}>
+          <Route exact path ='/'>
+            <Login/>
+          </Route>
+          <Route path ='/login'>
+            <Login/>
+          </Route>
+          <Route path ='/home'>
+            <Home/>
+          </Route>
+        </div>
+      </Switch>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
